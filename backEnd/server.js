f22b897136173b098ea85c5dd9c7f57f10c5c45a
James Chang <jamesbchang@gmail.com>
2022-07-18 20:19:52 +0900

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,18 +1,21 @@
 var express = require( 'express' );
+const bodyParser = require('body-parser');
 var cors = require( 'cors' );
 var app = express();
 
+var databaseTemp = [];  // 서버가 돌아갈동안 살아있는 정보...
+
 //app.use( cors() );
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 // GET API #1 - 단순 테스트
 app.get( '/basic', function( req, res) {
 
    // database에서 가저온 유저 정보:
    var dbData = 'james test user list count: 5'; 
-
-   console.log( 'test' );
-   console.log( dbData );
    
    res.send( 'Hi, this is backend for simpleJS. 지금시간: ' + new Date().toDateString() + ', 다른정보: ' + dbData );
 });
@@ -23,6 +26,33 @@ app.get('/getIndex', function (req, res) {
    console.log( 'test' );
    console.log( __dirname );
    res.sendFile( __dirname + "/public/" + "index.html" );
+});
+
+
+// GET API #4 - 회원가입
+app.post('/memberSignUp', function (req, res) {
+
+   // check if this person is already in the list <-- (X)
+
+   // req.body.---
+   console.log( req.body );
+
+   var newUser = req.body;
+   
+   //var newUser = {
+   //   name: req.body.name,
+   //   age: req.body.age
+   //};
+
+   databaseTemp.push( newUser );
+
+   res.sendStatus(200);
+});
+
+// GET API #5 - 회원들 정보
+app.get('/members', function (req, res) {
+
+   res.end(JSON.stringify(databaseTemp));
 });
 
 
